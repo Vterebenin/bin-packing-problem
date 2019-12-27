4a4e3680824b0fcc977374641ed22e1ea8e2bb43
Vterebenin <terebenin.work@gmail.com>
2019-12-27 13:43:02 +0300

    working solution

--- containers.py
+++ containers.py
@@ -1,52 +1,118 @@
+"""
+Генетический алгоритм решения задачи об упаковке в контейнеры
+1) Сначала создаем первую случайную популяцию
+...
+"""
+import operator
 import random
+import numpy as np
 
 
 class Container(object):
-    """ Контейнер с сохранением его суммы """
+    """ Контейнер(особь) с сохранением его суммы """
     def __init__(self):
-        self.items = []
+        self.fitted_items = []
+        self.current_items = []
         self.sum = 0
+        self.chromosome = []
+        self.activation = 0
 
-    def append(self, item):
-        self.items.append(item)
-        self.sum += item
+    def set_params(self):
+        indexes = []
+        for gen_index, gen in enumerate(self.chromosome):
+            if gen == 1:
+                indexes.append(gen_index)
+        self.fitted_items = [item for i, item in enumerate(self.current_items) if i in indexes]
+        self.sum = sum(self.fitted_items)
+        self.activation = len(self.fitted_items)
+
+    def mutate(self):
+        self.chromosome = [np.logical_not(gen).astype(int) for gen in self.chromosome]
+        self.set_params()
+
+    def limitation(self):
+        self.set_params()
+        return self.sum <= 1
 
     def __str__(self):
         """ Представление для печати """
-        return 'Контейнер(сумма=%s, предметы=%s)' % (self.sum, str(self.items))
+        return 'Контейнер(сумма=%s, хромосома=%s предметы=%s)' % (self.sum, str(self.chromosome), str(self.fitted_items))
 
 
-def pack(values, max_value):
-    values = sorted(values, reverse=True)
+def generate_first_population(values):
+    """
+    генерация первой, случайной, популяции
+    возвращает список контейнеров
+    """
     containers = []
+    population_count = 50
 
-    for item in values:
-        # Попытка влезть в контейнер
-        for container in containers:
-            if container.sum + item <= max_value:
-                container.append(item)
-                break
-        else:
-            # Предмет не влез ни в 1 контейнер, создаем новый контейнер
-            container = Container()
-            container.append(item)
-            containers.append(container)
+    for _ in range(population_count):
+        container = Container()
+        container.current_items = values
+        container.chromosome = [random.randrange(2) for _ in values]
+        containers.append(container)
+
+    containers = [container for container in containers if container.limitation()]
+    containers = sort_population(containers)
     return containers
 
 
-def pack_and_show(a_list, max_value):
+def crossbreeding(containers):
+    for key, container in enumerate(containers):
+        try:
+            parent_1 = container
+            parent_2 = containers[key + 1]
+            child_1 = Container()
+            child_1.chromosome = parent_1.chromosome[:len(parent_1.chromosome)//2] + \
+                    parent_2.chromosome[len(parent_2.chromosome) // 2:]
+            child_2 = Container()
+            child_2.chromosome = parent_2.chromosome[:len(parent_2.chromosome)//2] + \
+                      parent_1.chromosome[len(parent_1.chromosome) // 2:]
+            containers.append(child_1)
+            containers.append(child_2)
+            if key % 10 == 0:
+                container.mutate()
+            containers = sort_population(containers)
+            containers = containers[:50]
+        except IndexError:
+            pass
+    containers = [container for container in containers if container.limitation()]
+    containers = sort_population(containers)
+    return containers
+
+
+def sort_population(population):
+    population.sort(key=operator.attrgetter('activation'), reverse=True)
+    return population
+
+
+def run_generations(a_list):
     """ Упаковать предметы в контейнеры и показать результат """
-    print(f'Список с суммой {sum(a_list)} требует минимально {(sum(a_list)+max_value-1)/max_value} контейнеров')
+    generations = 30
 
-    containers = pack(a_list, max_value)
+    best_containers = []
+    for generation in range(generations):
+        containers = generate_first_population(a_list)
+        containers = crossbreeding(containers)
+        best_container = containers[:50][0] if containers else 0
 
-    print(f'Решение получено, необходимо контейнеров -- {len(containers)}')
-    for container in containers:
+        if best_container == 0:
+            break
+        best_containers.append(best_container)
+        indexes = []
+        for gen_index, gen in enumerate(best_container.chromosome):
+            if gen == 1:
+                indexes.append(gen_index)
+        a_list = [item for i, item in enumerate(a_list) if i not in indexes]
+        if not a_list:
+            break
+
+    print(f'Получилось {len(best_containers)} контейнеров:')
+    for container in best_containers:
         print(container)
 
 
-random_list = [random.uniform(0, 1) for i in range(100)]
-pack_and_show(random_list, 1)
+random_list = [random.uniform(0, 0.004) for i in range(1000)]
+run_generations(random_list)
 
-non_random_list = [0.1, 0.3, 0.4, 0.7, 0.2, 0.1]
-pack_and_show(non_random_list, 1)
